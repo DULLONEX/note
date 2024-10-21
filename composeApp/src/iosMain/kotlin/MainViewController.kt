3326b04c9f3ce127e53229b579ad542560072986
buhuazhen <hua100783@gmail.com>
2024-10-21 14:22:28 +0800

    解决ios 切换深色模式startKoin 重复执行导致程序崩溃问题

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -2,10 +2,11 @@ import androidx.compose.ui.window.ComposeUIViewController
 import androidx.navigation.compose.rememberNavController
 import data.Database
 import data.IOSDatabaseDriverFactory
+import org.koin.core.context.KoinContext
 import org.koin.core.context.startKoin
 import org.koin.core.parameter.parametersOf
 import platform.UIKit.UIViewController
-
+private var isKoinStarted = false
 
 fun ComposeEntryPointWithUIViewController(
     createUIViewController: () -> UIViewController
@@ -16,11 +17,14 @@ fun ComposeEntryPointWithUIViewController(
     }
 
     val navController = rememberNavController()
-    startKoin {
-        modules(iosModule)
-    }.also {
-        it.koin.get<Platform> { parametersOf(navController) }
-        it.koin.get<Database> { parametersOf(IOSDatabaseDriverFactory()) }
+    if (!isKoinStarted) {
+        startKoin {
+            modules(iosModule)
+        }.also {
+            it.koin.get<Platform> { parametersOf(navController) }
+            it.koin.get<Database> { parametersOf(IOSDatabaseDriverFactory()) }
+        }
+        isKoinStarted = true
     }
 
     App()
